2e891b899c21274b7aca3018f21dcaa6f48eda7d
henwh0 <hmcginnis96@gmail.com>
2025-12-23 08:43:36 -0500

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -54,24 +54,27 @@ def play_game():
     while not game_over:
         user_score = calculate_score(user_cards)
         computer_score = calculate_score(computer_cards)
-        print(f"Your cards: {user_cards}, Your current score: {user_score}")
-        print(f"Computers first card: {computer_cards[0]}")
+        print(f"Your cards: {user_cards}, Your current score: {user_score}\n")
+        print(f"Computers first card: {computer_cards[0]}\n")
 
         if user_score == 0 or computer_score == 0 or user_score > 21:
             game_over = True
         else:
-            deal_more_cards = input("Type 'y' to get another card, type 'n' to pass: ")
-            if deal_more_cards == "y":
+            deal_more_cards = input("Type 'hit' to get another card, type 'stand' to pass: ")
+            if deal_more_cards == "hit":
                 user_cards.append(deal_card())
-            else:
+            elif deal_more_cards == "stand":
                 game_over = True
+            else:
+                print("Invalid input.. Try again")
+                continue
 
     while computer_score != 0 and computer_score < 17:
         computer_cards.append(deal_card())
         computer_score = calculate_score(computer_cards)
 
-    print(f"Your final hand: {user_cards}, final score: {user_score}")
-    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(f"Your final hand: {user_cards}, final score: {user_score}\n")
+    print(f"Computer's final hand: {computer_cards}, final score: {computer_score}\n")
     print(compare(user_score, computer_score))
 
 
